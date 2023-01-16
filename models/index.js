8bb026f841a758b854cfb9908118d9b3d2ccf89b
BelenGigante <vinn_a@hotmail.com>
2023-01-16 07:59:08 -0500

    finished models index not tested

--- models/index.js
+++ models/index.js
@@ -5,15 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
-  foreignKey: '',
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag,
+    unique: false,
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through:{
+    model: ProductTag,
+    unique: false,
+  }
+})
 
 module.exports = {
   Product,
