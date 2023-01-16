dc18e7909937e52f736474c734a785f8354e3db6
BelenGigante <vinn_a@hotmail.com>
2023-01-16 08:03:30 -0500

    finished models category not tested

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,10 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
